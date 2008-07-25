2421d153304e97315b8e4e65a25802633a5d204a
Tomáš Müller <muller@unitime.org>
2008-07-25 16:05:13 +0000

    fixed NonUniqueObjectException (a different object with the same identifier value was already associated with the session: [org.unitime.timetable.model.Building#?])
    This exception was thrown when hibernate decided to update session (toSession) during save of a newly roll-forwarded room -- caused by the existence of a different instance of the same building in the PersistentContext:
    	room -> building (instance loaded by a finder method using abbv/external id of the last-like building)
    		- This is the instance referred in PersistenContext
    	room -> session -> buildings (instance created in method rollForwardBuildings, but evicted from the hibernate session in the the same method rollForwardBuildings)
    		- This is the instance causing the exception (evicted from PersistentContext in rollForwardBuildings)
    
    other potential approached to fix such an issue:
    	- using session.evict with care -- (all objects that have a cascading relation to the evicted object SHOULD BE evicted as well)
    	- change cascading relation (not update session when a room is updated, or not update session.buildings when a session is updated etc)
    	- remove relations session -> buildings, rooms, subjectAreas etc. (they are barely used and may cause hibernate cache update problems)
    
    
    Former-commit-id: 5595a6b74851edca102554a7e97c80f017b9e881

--- JavaSource/org/unitime/timetable/util/SessionRollForward.java
+++ JavaSource/org/unitime/timetable/util/SessionRollForward.java
@@ -4436,16 +4436,7 @@ operator|.
 name|flush
 argument_list|()
 expr_stmt|;
-name|bDao
-operator|.
-name|getSession
-argument_list|()
-operator|.
-name|evict
-argument_list|(
-name|toBldg
-argument_list|)
-expr_stmt|;
+comment|//bDao.getSession().evict(toBldg); -- commented out to prevent NonUniqueObjectException
 name|bDao
 operator|.
 name|getSession
