a874fdab9646691a689ce77854cf37872f562acd
says@unitime.org <says@unitime.org>
2011-02-23 14:15:57 +0000

    Correct spellings in labels.
    Former-commit-id: a11cf0cab7c42aef243b69c2e83c4d7ada9522b4

--- JavaSource/org/unitime/timetable/action/RollForwardSessionAction.java
+++ JavaSource/org/unitime/timetable/action/RollForwardSessionAction.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * UniTime 3.2 (University Timetabling Application)  * Copyright (C) 2008 - 2010, UniTime LLC, and individual contributors  * as indicated by the @authors tag.  *   * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *   * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *   * You should have received a copy of the GNU General Public License along  * with this program.  If not, see<http://www.gnu.org/licenses/>.  *  */
+comment|/*  * UniTime 3.2 (University Timetabling Application)  * Copyright (C) 2008 - 2011, UniTime LLC, and individual contributors  * as indicated by the @authors tag.  *   * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *   * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *   * You should have received a copy of the GNU General Public License along  * with this program.  If not, see<http://www.gnu.org/licenses/>.  *  */
 end_comment
 
 begin_package
@@ -2188,7 +2188,7 @@ name|names
 operator|.
 name|add
 argument_list|(
-literal|"roons"
+literal|"rooms"
 argument_list|)
 expr_stmt|;
 if|if
@@ -2286,7 +2286,7 @@ name|names
 operator|.
 name|add
 argument_list|(
-literal|"midter exams"
+literal|"midterm exams"
 argument_list|)
 expr_stmt|;
 if|if
