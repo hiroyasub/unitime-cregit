b9736cc4d4c0b48fa06871244b5c9b4a8ec27a1a
Tomáš Müller <muller@unitime.org>
2011-05-16 17:44:17 +0000

    Version changed for trunk to UniTime 3.3.
    Former-commit-id: 2437b22a0bd02e37b7bf89ba2bd2f743585d39be

--- JavaSource/org/unitime/timetable/gwt/client/ToolBox.java
+++ JavaSource/org/unitime/timetable/gwt/client/ToolBox.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * UniTime 3.2 (University Timetabling Application)  * Copyright (C) 2010, UniTime LLC, and individual contributors  * as indicated by the @authors tag.  *   * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *   * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *   * You should have received a copy of the GNU General Public License along  * with this program.  If not, see<http://www.gnu.org/licenses/>.  *  */
+comment|/*  * UniTime 3.3 (University Timetabling Application)  * Copyright (C) 2010 - 2011, UniTime LLC, and individual contributors  * as indicated by the @authors tag.  *   * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *   * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *   * You should have received a copy of the GNU General Public License along  * with this program.  If not, see<http://www.gnu.org/licenses/>.  *  */
 end_comment
 
 begin_package
@@ -258,7 +258,7 @@ argument_list|()
 operator|+
 literal|"images/timetabling.ico\">"
 operator|+
-literal|"<title>UniTime 3.2 | University Timetabling Application</title>"
+literal|"<title>UniTime 3.3 | University Timetabling Application</title>"
 operator|+
 literal|"</header><body>"
 operator|+
@@ -303,7 +303,7 @@ literal|"</td></tr><tr><td>"
 operator|+
 literal|"<table class=\"unitime-Footer\"><tr>"
 operator|+
-literal|"<td width=\"33%\" align=\"left\" nowrap=\"nowrap\">Printed from UniTime 3.2 | University Timetabling Application</td>"
+literal|"<td width=\"33%\" align=\"left\" nowrap=\"nowrap\">Printed from UniTime 3.3 | University Timetabling Application</td>"
 operator|+
 literal|"<td width=\"34%\" align=\"center\">&copy; 2008 - 2011 UniTime LLC</td>"
 operator|+
