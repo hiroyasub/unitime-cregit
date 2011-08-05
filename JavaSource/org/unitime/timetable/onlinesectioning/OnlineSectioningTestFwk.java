f81e7b121dcbe75e52709c52f5094d9ca52ea02d
Tomáš Müller <muller@unitime.org>
2011-08-05 13:28:40 +0000

    Online Student Sectioning test framework  - disable logging
    Former-commit-id: 32b62861932919b7abe8bc8dc6ff8e82a1981b8b

--- JavaSource/org/unitime/timetable/onlinesectioning/OnlineSectioningTestFwk.java
+++ JavaSource/org/unitime/timetable/onlinesectioning/OnlineSectioningTestFwk.java
@@ -467,6 +467,16 @@ name|session
 argument_list|)
 expr_stmt|;
 block|}
+name|OnlineSectioningLogger
+operator|.
+name|getInstance
+argument_list|()
+operator|.
+name|setEnabled
+argument_list|(
+literal|false
+argument_list|)
+expr_stmt|;
 name|iServer
 operator|=
 operator|new
