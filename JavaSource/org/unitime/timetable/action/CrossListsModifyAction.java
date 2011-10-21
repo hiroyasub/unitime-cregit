b10c5c80bc57b3c41b196520fe4d03f024ee8eab
Tomáš Müller <muller@unitime.org>
2011-10-21 19:27:53 +0000

    Instructional Offering Cross Lists  - also allow a course that is offered, but not cross-listed and with no configuration to be added into a cross-list    - this can be for instance a newly added course (so there is no need to make the newly created course not offered)
    This extends the check from revision 2642.
    
    
    Former-commit-id: 99e53df73673023cddf156a202c6bb0c252353bb

--- JavaSource/org/unitime/timetable/action/CrossListsModifyAction.java
+++ JavaSource/org/unitime/timetable/action/CrossListsModifyAction.java
@@ -1230,6 +1230,7 @@ argument_list|()
 decl_stmt|;
 if|if
 condition|(
+operator|(
 operator|!
 name|co
 operator|.
@@ -1238,6 +1239,19 @@ argument_list|()
 operator|.
 name|isNotOffered
 argument_list|()
+operator|&&
+operator|!
+name|co
+operator|.
+name|getInstructionalOffering
+argument_list|()
+operator|.
+name|getInstrOfferingConfigs
+argument_list|()
+operator|.
+name|isEmpty
+argument_list|()
+operator|)
 operator|||
 name|co
 operator|.
