cd73dd0f68e8154709c55f4a8cdc5f2efbebee38
Tomáš Müller <muller@unitime.org>
2013-03-05 21:23:44 +0000

    Logging  - save logging messages into MessageLog table  - messages are kept for 14 days (can be changed by custom property unitime.message.log.cleanup.days)  - only messages of level warning and higher are saved (can be changed by custom property unitime.message.log.level)  - there is a new report Message Log under Administration > Academic Session > Reports
    Former-commit-id: b2fb86422ebbd652d0cb6059908d202021d045cc

--- JavaSource/org/unitime/timetable/gwt/client/hql/SavedHQLPage.java
+++ JavaSource/org/unitime/timetable/gwt/client/hql/SavedHQLPage.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * UniTime 3.3 (University Timetabling Application)  * Copyright (C) 2011, UniTime LLC, and individual contributors  * as indicated by the @authors tag.  *   * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *   * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *   * You should have received a copy of the GNU General Public License along  * with this program.  If not, see<http://www.gnu.org/licenses/>.  *  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -586,24 +590,6 @@ operator|.
 name|ui
 operator|.
 name|HasVerticalAlignment
-import|;
-end_import
-
-begin_import
-import|import
-name|com
-operator|.
-name|google
-operator|.
-name|gwt
-operator|.
-name|user
-operator|.
-name|client
-operator|.
-name|ui
-operator|.
-name|Label
 import|;
 end_import
 
@@ -4858,9 +4844,22 @@ operator|.
 name|add
 argument_list|(
 operator|new
-name|Label
+name|HTML
 argument_list|(
 name|x
+operator|==
+literal|null
+condition|?
+literal|""
+else|:
+name|x
+operator|.
+name|replace
+argument_list|(
+literal|"\\n"
+argument_list|,
+literal|"<br>"
+argument_list|)
 argument_list|)
 argument_list|)
 expr_stmt|;
