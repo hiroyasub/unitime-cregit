42882e8fa32eded1c62355b25d0559f14295f0a3
Tomáš Müller <muller@unitime.org>
2014-03-10 12:54:21 +0000

    Solver Parameters  - solver parameter names do not need to be unique, they only need to be unique within a problem type (e.g., course and examination timetabling may share some parameter names)
    Former-commit-id: 341e2835574e4529cb0658ed2c44f5bf5eeaa216

--- JavaSource/org/unitime/timetable/form/SolverParamDefForm.java
+++ JavaSource/org/unitime/timetable/form/SolverParamDefForm.java
@@ -238,9 +238,11 @@ name|def
 init|=
 name|SolverParameterDef
 operator|.
-name|findByName
+name|findByNameGroup
 argument_list|(
 name|name
+argument_list|,
+name|group
 argument_list|)
 decl_stmt|;
 if|if
