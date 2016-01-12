770756e14e13b0df6abbfbd42aa21e0cd965c797
tomas-muller <muller@unitime.org>
2016-01-12 13:54:05 +0100

    Copyright: 2016
    - copyright message updated to include 2016
    
    
    Former-commit-id: 51b7302a2488d70f2bdb9032947f602fb372b9ad

--- JavaSource/org/unitime/timetable/gwt/resources/GwtMessages.java
+++ JavaSource/org/unitime/timetable/gwt/resources/GwtMessages.java
@@ -58,7 +58,7 @@ function_decl|;
 annotation|@
 name|DefaultMessage
 argument_list|(
-literal|"&copy; 2008 - 2015 The Apereo Foundation,<br>distributed under the Apache License, Version 2."
+literal|"&copy; 2008 - 2016 The Apereo Foundation,<br>distributed under the Apache License, Version 2."
 argument_list|)
 name|String
 name|pageCopyright
@@ -67,7 +67,7 @@ function_decl|;
 annotation|@
 name|DefaultMessage
 argument_list|(
-literal|"UniTime {0}, \u00A9 2008 - 2015 The Apereo Foundation, distributed under the Apache License."
+literal|"UniTime {0}, \u00A9 2008 - 2016 The Apereo Foundation, distributed under the Apache License."
 argument_list|)
 name|String
 name|pdfCopyright
