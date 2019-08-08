9df1950e77d62c74e0deab73e536f3686a67e0ef
tomas-muller <muller@unitime.org>
2019-08-08 19:52:33 +0200

    Grade Modes: Disclaimer
    - added ability to put in a custom disclaimer for each grade mode change
    - application property purdue.specreg.gradeModeDisclaimer.X, where X is the code of the grade mode change
    
    
    Former-commit-id: 8b634f57d74a7f5ae588a50b905d4d7794e8ef9b

--- JavaSource/org/unitime/timetable/gwt/resources/StudentSectioningMessages.java
+++ JavaSource/org/unitime/timetable/gwt/resources/StudentSectioningMessages.java
@@ -7429,6 +7429,15 @@ function_decl|;
 annotation|@
 name|DefaultMessage
 argument_list|(
+literal|"\n<b>Disclaimer:</b>"
+argument_list|)
+name|String
+name|gradeModeDisclaimers
+parameter_list|()
+function_decl|;
+annotation|@
+name|DefaultMessage
+argument_list|(
 literal|"Not Set"
 argument_list|)
 name|String
