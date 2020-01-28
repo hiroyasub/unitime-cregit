ba262d196088f348c308da579f83561706b0593b
tomas-muller <muller@unitime.org>
2020-01-28 15:51:59 +0100

    UniTime 4.5
    UniTime version on the master branch changed to 4.5
    
    
    Former-commit-id: 637e3751e069e9747870b42817754b8f7594daff

--- JavaSource/org/unitime/timetable/gwt/resources/GwtConstants.java
+++ JavaSource/org/unitime/timetable/gwt/resources/GwtConstants.java
@@ -31,7 +31,7 @@ block|{
 annotation|@
 name|DefaultStringValue
 argument_list|(
-literal|"4.4"
+literal|"4.5"
 argument_list|)
 annotation|@
 name|DoNotTranslate
--- JavaSource/org/unitime/timetable/util/Constants.java
+++ JavaSource/org/unitime/timetable/util/Constants.java
@@ -490,7 +490,7 @@ specifier|static
 name|String
 name|VERSION
 init|=
-literal|"4.4"
+literal|"4.5"
 decl_stmt|;
 comment|/** release date */
 specifier|private
